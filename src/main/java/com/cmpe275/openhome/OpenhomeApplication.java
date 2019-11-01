7e1207e8f3c5be6937d2cb31fa535c14cf47d31a
surabhisinha31 <surabhi.sinha@sjsu.edu>
2019-11-01 00:47:00 -0700

    Initila commit for adding boilercode for openhome project

---
+++ src/main/java/com/cmpe275/openhome/OpenhomeApplication.java
@@ -0,0 +1,13 @@
+package com.cmpe275.openhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenhomeApplication.class, args);
+	}
+
+}
